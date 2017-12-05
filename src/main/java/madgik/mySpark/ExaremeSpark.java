590ca8b8a006934c4b19cdc82290f1d691201b05
nkaralis <nkaralis@di.uoa.gr>
2017-12-05 10:13:18 +0200

    added TextWindow vtable

--- src/main/java/madgik/mySpark/ExaremeSpark.java
+++ src/main/java/madgik/mySpark/ExaremeSpark.java
@@ -1,7 +1,12 @@
 package madgik.mySpark;
 
+import java.util.ArrayList;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.sql.api.java.UDF4;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
 import org.jline.reader.EndOfFileException;
 import org.jline.reader.LineReader;
 import org.jline.reader.UserInterruptException;
@@ -26,8 +31,20 @@ public class ExaremeSpark {
 				ExaremeSparkSession spark =  ExaremeSparkSession.exaremebuild()
 						.master("local[1]")
 						.appName("ExaremeSpark")
-						.getOrCreateExareme();   
+						.getOrCreateExareme();
 				
+				/* textwindow2s registration */
+//				spark.getSparkSession().udf().register("textwindow2s", new UDF4<String, Integer, Integer, Integer, ArrayList<ArrayList<String>>>(){
+//					@Override
+//					public ArrayList<ArrayList<String>> call(String s, Integer prev, Integer mid, Integer next){
+//						String[] tokens = s.split(" ");
+//						for(int i = 0; i < tokens.length-mid+1; i++){
+//							int im = i+mid;
+//							
+//						}
+//						return "hello";
+//					}
+//				}, DataTypes.StringType);
 				
 				String query;
 				try{
