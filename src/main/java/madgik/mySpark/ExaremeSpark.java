f419b3eca7987afcd441a80e25ba6f3c1fbe86a2
jJImo <dimitrisspyr@gmail.com>
2018-01-17 21:09:31 +0200

    Added udf regxpcountwords,according to python code for it in repository madgik recital. tested for the example cases given in text.py file

--- src/main/java/madgik/mySpark/ExaremeSpark.java
+++ src/main/java/madgik/mySpark/ExaremeSpark.java
@@ -12,6 +12,9 @@ import madgik.mySpark.parser.ParserUtils;
 import madgik.mySpark.parser.exception.VtExtensionParserException;
 import madgik.mySpark.udaf.Joinstr;
 import org.apache.spark.sql.api.java.UDF2;
+import org.apache.commons.lang3.StringUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 ///
 
 public class ExaremeSpark {
@@ -34,6 +37,7 @@ public class ExaremeSpark {
 				
 				spark.getSparkSession().udf().register("Normtitles",Normtitles,DataTypes.StringType);
 				spark.getSparkSession().udf().register("Comprspaces",Comprspaces,DataTypes.StringType);
+				spark.getSparkSession().udf().register("Regexpcountwords",Regexpcountwords,DataTypes.IntegerType);
 				spark.getSparkSession().udf().register("Joinstr", new Joinstr());
 				String query;
 				try{
@@ -67,6 +71,8 @@ public class ExaremeSpark {
 			return str.toLowerCase().replaceAll("[^a-zA-z0-9 ]","_").replaceAll(" +"," ");
 		}
 	};
+	//select comprspaces(line,"NULL") from (select * from foo(file for foo to read to dataset containing only one field with value line)
+	//second argument NULL for the needs of the execercise
 	private static UDF2<String,String,String> Comprspaces = new UDF2<String,String,String>(){
 		@Override
 		public String call(String arg0, String arg1) throws Exception {
@@ -77,9 +83,28 @@ public class ExaremeSpark {
 	            return arg0.trim().replaceAll(" +"," ") +" "+ arg1.trim().replaceAll(" +"," ");
 		}
 	};
+	private static UDF2<String,String,Integer> Regexpcountwords = new UDF2<String,String,Integer>(){
+		@Override
+		public Integer call(String arg0, String arg1) throws Exception {
+			 //arg0 ->  pattern
+			 //arg1 -> expression
+			 Pattern myPattern = Pattern.compile(arg0, Pattern.UNICODE_CHARACTER_CLASS);
+             Matcher myMatcher = myPattern.matcher(arg1);
+             int sum = 0;
+             int occurences ;
+             while (myMatcher.find()) {
+                   // Do your stuff here
+                   occurences = 0; 
+                   System.out.println(myMatcher.group());
+                   occurences = StringUtils.countMatches(myMatcher.group().trim()," ") + 1;
+                   sum = sum + occurences;
+                   //sum = sum + myMatcher.group().trim().count(' ')+1;
+             }
+             return sum;
+		}
+	};
 	
-	
-	
+	//select regexpcountwords('start',line) from (select * from foo(',','../demospaces.txt'))
 	
 	
 	
